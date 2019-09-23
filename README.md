7b9a796bca31978b2c444595ef42835deeb3556e
Philipp B. Costa <philcoder@gmail.com>
2019-09-23 17:07:05 -0300

    how to dev quicksort using erlang

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
-# learn erlang
+Learning Erlang
+====================================================================
 
-PS: use these commands inside erlang docker runtime...
+This guide will provide you all the steps to run Erlang language inside docker image.
+
+
+### System Requeriments:
+
+1. Components docker and docker-compose already installed on developer machine
+
+### Start/Stop Docker services
+
+For startup all services
+```
+docker-compose -f docker-compose.yml up -d --build
+```
+
+For stop all services
+```
+docker-compose -f docker-compose.yml down
+```
+
+For stop and cleanup volumes 
+```
+docker-compose -f docker-compose.yml down --volumes
+```
+
+Join inside docker image
+```
+docker exec -it app bash
+```
+
+### Inside Docker image for execute erlang code
 
 -- For compile a erlang file (at same directory of file)
 erlc <name_file>
@@ -11,3 +41,10 @@ e.g.: erlc hello.erl (execute it inside cap1 folder)
 erl -noshell -s <name_file> <start_method> -s init stop
 
 e.g.: erl -noshell -s hello hello_world -s init stop
+
+
+<b>PS: use for speedup that process use command</b>
+```
+make all
+```
+<b>Inside Makefile change "main" script to call as you wish</b>
--- app/code/cap4/lang_features.erl
+++ app/code/cap4/lang_features.erl
@@ -8,7 +8,7 @@
 -export([main/0]).
 
 main() -> 
-    func4().
+    func8().
 
 func1() ->
     ResultSquare = area({square, 9}),
@@ -69,6 +69,93 @@ func4() ->
     for(1, 5, fun(E) -> io:fwrite("hello world loop\n") end),
 
     fend.
+
+func5() ->
+    Elems = [1,2,3,4,5],
+    %funcao anonima
+    Sum = fun AnnoFuncSum([H|T]) -> H + AnnoFuncSum(T); %onde percorre a list pela recursao
+            AnnoFuncSum([]) -> 0  %cond de parada
+    end,
+    io:fwrite("~w\n",[Sum(Elems)]),
+
+    Map = fun AnnoFuncMap(_, []) -> [];
+            AnnoFuncMap(Func, [H|T]) -> [Func(H) | AnnoFuncMap(Func, T)]
+    end,
+    ResultMap = Map(fun(E) -> E * 3 end, Elems),
+    io:fwrite("~w\n",[ResultMap]),
+
+    %uma funcao dentro da outra
+    Total = Sum(Map(fun(E) -> E * 5 end, Elems)),
+    io:fwrite("~w\n",[Total]),
+
+    fend.
+
+func6() ->
+    Elems = [1,2,3,4,5],
+
+    %map usando a lib do erlang
+    ResultMap = lists:map(fun(E) -> E * 2 end, Elems),
+    io:fwrite("~w\n",[ResultMap]),
+
+    %uma forma de realizar um 'map' de forma mais simples
+    %o nome desse processo é 'list comprehensions'
+    ResultMapCompact = [E * 3 || E <- Elems],
+    io:fwrite("~w\n",[ResultMapCompact]),
+
+    %usando com elementos mais complexos
+    BuyList=[{oranges,4},{newspaper,1},{apples,10},{pears,6},{milk,3}],
+    ResultComplexElems = [{Name, 3*Quantity} || {Name, Quantity} <- BuyList],
+    io:fwrite("~w\n",[ResultComplexElems]),
+
+    PriceElems = [cost(Name) || {Name, Quantity} <- BuyList],
+    io:fwrite("~w\n",[PriceElems]),
+
+    Total = lists:sum([cost(Name) * Quantity || {Name, Quantity} <- BuyList]),
+    io:fwrite("Total Elements: ~w\n",[Total]),
+
+    %exemplo de filter usando tuplas no caso elem 'a'
+    Result = [ X || {a, X} <- [{a,1},{b,2},{c,3},{a,4},hello,"wow"]],
+    io:fwrite("Filter Tuples 'a' content: ~w\n",[Result]),
+
+    fend.
+
+func7() ->
+    Elems = [23,6,2,-9,27,400,78,-45,61,82,14],
+
+    %exemplo elegante de quicksort
+    %essa linha que tem "++ [Pivot] ++" concat o elemento na lista
+    
+    %esse algoritmo pega o pivot = 23 e seleciona todos os elementos menores que ele e todos os maiores
+    %ordena esses elementos e na volta da recursão vai concat todos os elementos em ordem crescente...
+    QSort = fun QuickSort([]) -> [];
+            QuickSort([Pivot|Tail]) -> QuickSort([X || X <- Tail, X < Pivot])
+            ++ [Pivot] ++
+            QuickSort([X || X <- Tail, X >= Pivot]) %condição de filtro da 'list comprehensions'
+    end,
+
+    Result = QSort(Elems),
+    io:fwrite("Original list: ~w\n",[Elems]),
+    io:fwrite("Ordered list: ~w\n",[Result]),
+
+    fend.
+
+func8() ->
+    %Pythagorean triplets are sets of integers {A,B,C} where A^2 + B^2 = C^2 and where the sum of the sides is less than or equal to N .
+    Pythag = fun Pythagorean(Num) ->
+        [
+            {A ,B, C} ||
+            A <- lists:seq(1,Num), %cria uma lista de todos os numeros de 1 até N
+            B <- lists:seq(1,Num), 
+            C <- lists:seq(1,Num),
+            A + B + C =< Num, % condição do filtro
+            A*A + B*B =:= C*C % 'e' condição do filtro
+        ]
+    end,
+
+    io:fwrite("Original list: ~w\n",[Pythag(16)]),
+
+    fend.
+
 
 %exemplo de loop 'for' em erlang
 for(Max, Max, F) -> [F(Max)]; %quando Init == Max ai para a recursão
